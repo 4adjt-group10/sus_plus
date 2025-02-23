9b02e867f5dbf5c55953b442166500286245cb4c
marcos <marcos_mol@hotmail.com>
2025-02-23 17:28:06 -0300

    Feature: feito a comunicação entre os serviços comr abbit mq , unity to manager , scheduler to integrated , scheduler to unity , feito correção em nome de pacote scheduler estava como integrated

--- sus_unity/src/main/java/br/com/susunity/service/UnityService.java
+++ sus_unity/src/main/java/br/com/susunity/service/UnityService.java
@@ -135,7 +135,7 @@ public class UnityService {
         unityRepository.findById(message.unityId())
                 .ifPresentOrElse(
                         unityModel -> {
-                            boolean isSpecialityValid = validateEpeciality(unityModel, message);
+                            boolean isSpecialityValid = validateEspeciality(unityModel, message);
                             messageProducer.sendToScheduler(new MessageBodyForScheduler(isSpecialityValid, true, message.schedulingId()));
                         },
                         () -> messageProducer.sendToScheduler(new MessageBodyForScheduler(false, false, message.schedulingId()))
@@ -143,7 +143,7 @@ public class UnityService {
     }
 
 
-    private boolean validateEpeciality(UnityModel unityModel, MessageBodyForUnity message) {
+    private boolean validateEspeciality(UnityModel unityModel, MessageBodyForUnity message) {
         if (unityModel.getProfessional().isEmpty()) {
             return false;
         }
